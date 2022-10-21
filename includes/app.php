0ac649657a928673434a4610e6a76089e46bcbdb
Juan Q <jmquijano@unimayor.edu.co>
2022-10-20 22:31:44 -0500

    Deploy

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,12 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
 require 'database.php';
 
-use Model\ActiveRecord;
 //Conexion a BD
+use Model\ActiveRecord;
 ActiveRecord::setDB($db);
