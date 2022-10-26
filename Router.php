bea4328ad7b8a212d8323c652b647e4762d7081c
Juan Q <jmquijano@unimayor.edu.co>
2022-10-25 20:06:50 -0500

    Cambios

--- Router.php
+++ Router.php
@@ -22,7 +22,7 @@ class Router
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
-        debuguear($currentUrl);
+        debuguear($method);
 
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
